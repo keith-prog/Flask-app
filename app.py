ccca9171a6cc4e6a830321f5f726d6e2006b10c4
Keith Davidson <c00252500@itcarlow.ie>
2021-10-26 11:28:27 +0100

    app.py formatted by black

--- app.py
+++ app.py
@@ -8,49 +8,55 @@ app = Flask(__name__)
 
 @app.get("/")  # HTTP request : GET  /
 def index():
-   # return datetime.datetime.now().ctime()
-   return render_template("index.html",  title="Home", heading="Home")
+    # return datetime.datetime.now().ctime()
+    return render_template("index.html", title="Home", heading="Home")
 
 
 @app.get("/hello")  # HTTP request  GET hello
 def hello():
     return "Hello from my first Flask WebApp : it is your fault"
 
+
 @app.get("/personal")
 def personal():
     pass
 
+
 @app.get("/CV")
 def display_CV():
-    
-     return render_template("Davidson_Keith_CV.pdf")
+
+    return render_template("Davidson_Keith_CV.pdf")
 
 
-#@app.route("/Showform", methods["GET", "POST", "PUT", "DELETE"])
+# @app.route("/Showform", methods["GET", "POST", "PUT", "DELETE"])
+
 
 @app.get("/showform")
 def display_form():
 
-    return render_template("form.html", title="Welcome", heading="TellUs About yourself")
+    return render_template(
+        "form.html", title="Welcome", heading="TellUs About yourself"
+    )
     """
     Retreive the form.html file from the hard disk, and send it to the browser.
     """
-    '''with open("form.html") as f:
+    """with open("form.html") as f:
         html = f.read()
-    return html'''
+    return html"""
+
+
 @app.post("/processform")
 def save_data():
     """
     Recieve the data from the html form then save it to a disk file. Then reopen with a message into the browser
     """
-    #python names = html names
-    the_first = request.form["first"]  
-    the_last = request.form["last"] 
-    the_dob =  request.form["dob"]
+    # python names = html names
+    the_first = request.form["first"]
+    the_last = request.form["last"]
+    the_dob = request.form["dob"]
     with open("comments.txt", "a") as sf:
         print(f"{the_first}, {the_last}, {the_dob}", file=sf)
-    return f"Thanks, {the_first}, we promise not to sell your data."     
-
+    return f"Thanks, {the_first}, we promise not to sell your data."
 
 
 if __name__ == "__main__":
